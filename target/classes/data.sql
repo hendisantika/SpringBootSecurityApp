61d028e82f71952af7c2d1b100c396770b0103bb
Hendi Santika <hendisantika@gmail.com>
2018-09-09 06:31:22 +0700

    #9 - Delete target folder

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO user (username, email, password, role)
-VALUES ('admin', 'admin@konohagakure.com', '$2a$06$vnBwhb2J1zeL3IpNLFGQ2OKHCekzkV3Au1AfC/Jn1.fT9sk2BcmWS', 'ADMIN'); // 123
-
-INSERT INTO user (username, email, password, role)
-VALUES ('hendisantika', 'hendisantika@konohagakure.com', '$2a$06$vnBwhb2J1zeL3IpNLFGQ2OKHCekzkV3Au1AfC/Jn1.fT9sk2BcmWS', 'USER'); // 123
-
-INSERT INTO user (username, email, password, role)
-VALUES ('demo', 'demo@localhost', '$2a$06$vnBwhb2J1zeL3IpNLFGQ2OKHCekzkV3Au1AfC/Jn1.fT9sk2BcmWS', 'USER'); // 123
